92a2daeb76093f17c1650292d4c79c3e8aaafbe6
Zero <zerocool@gmail.com>
2025-12-08 22:39:20 +0330

    js comment counter added

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 /*-----------------------------------*\
   #script.js
 \*-----------------------------------*/
+
+const textareaEL = document.querySelector(".form__textarea");
+const counterEL = document.querySelector(".counter");
+
+const inputHandler = () => {
+  const maxChars = 150;
+
+  const CharsTyped = textareaEL.value.length;
+
+  const charsLeft = maxChars - CharsTyped;
+
+  // number of charaters left
+  counterEL.textContent = charsLeft;
+};
+
+textareaEL.addEventListener("input", inputHandler);
+
+//submit component
+const formEL = document.querySelector(".form");
+
+const submitHandler = (event) => {
+  event.preventDefault();
+};
+
+textareaEL.addEventListener("submit", submitHandler);
